2ddb354bc0d6ebb7051f56c820efd17bb32ff586
sv-makh <mithriam@mail.ru>
2022-07-19 22:06:15 +0300

    32 min

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 
-class PacmanGame extends FlameGame with KeyboardHandler, HasCollisionDetection {
+class Background extends Component {}
+
+class Player extends Component {
+
+}
+
+class PacmanGame extends FlameGame with KeyboardEvents, HasCollisionDetection {
+  @override
+  Future<void>? onLoad() async {
+    this.add(
+        FpsTextComponent(position: Vector2(50, 50), size: Vector2(128, 32)));
+  }
 
 }
 
 void main() {
-  runApp(const GameWidget(game: PacmanGame(), ));
+  runApp(GameWidget(
+    game: PacmanGame(),
+  ));
 }
